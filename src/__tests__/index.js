8aeae737ab9f3558123308a31c2328547e607249
yacheng.sz <yacheng.sz@taobao.com>
2019-01-17 14:40:51 +0800

    feat: change name for jest

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,9 +1,9 @@
 import {createElement, Component} from 'rax';
 import renderer from 'rax-test-renderer';
-import BarCode from '../';
+import Pictrue from '../';
 
-describe('BarCode', () => {
-  it('test typeof BarCode', () => {
-    expect(typeof BarCode).toEqual('function');
+describe('Pictrue', () => {
+  it('test typeof Pictrue', () => {
+    expect(typeof Pictrue).toEqual('function');
   });
 });
