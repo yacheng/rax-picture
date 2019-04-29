3801c1d6dad4c5660b79c44612984e9427a2165f
yacheng.sz <yacheng.sz@taobao.com>
2019-04-29 10:12:59 +0800

    feat: 支持 setState 更新图片地址、resizemode 展示问题

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -3,10 +3,10 @@ import renderer from 'rax-test-renderer';
 import Pictrue from '../';
 
 jest.mock('rax-image', () => {
-    return function() {
-        return <img src="" alt="" />;
-    }
-})
+  return function() {
+    return <img src="" alt="" />;
+  };
+});
 
 describe('Pictrue', () => {
   it('test typeof Pictrue', () => {
