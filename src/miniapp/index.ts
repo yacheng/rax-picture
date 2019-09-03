fc69fec0ef79b5248116bd56381d68f6894e80d1
泠苞 <liaoshuai.ls@alibaba-inc.com>
2019-09-03 21:24:35 +0800

    fix: 小程序版本组件支持lazyload

--- src/miniapp/index.ts
+++ src/miniapp/index.ts
@@ -1,22 +1,14 @@
-'use strict';
-import fmtEvent from './fmtEvent';
-
 Component({
-  data: {},
   props: {
     className: '',
     style: '',
     source: {
       uri: ''
     },
+    resizeMode: 'contain',
     lazyload: false,
-    onClick: function onClick() {}
-  },
-  didMount: function didMount() {},
-  methods: {
-    onClick: function onClick(e) {
-      var event = fmtEvent(this.props, e);
-      this.props.onClick(event);
-    }
+    onClick: e => {},
+    onLoad: e => {},
+    onError: e => {}
   }
 });
